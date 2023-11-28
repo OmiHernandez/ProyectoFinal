b6352ff09725c853f903c5c3617a4c17d358d339
WallaceGaster <saitama.kurosakyy@gmail.com>
2023-11-28 12:35:11 -0600

    Veloz1-

---
+++ public_html/about.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MHVWare</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/1d83af7d53.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/1d83af7d53.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="css/about.css">
+    <link rel="stylesheet" href="css/custom.css">
+    <link rel="icon" type="image/x-icon" href="img/favicon.jpg">
+</head>
+
+<body>
+    <?php
+    include("login.php");
+    ?>
+
+    <section class="seccion-1">
+        <div class="contenedor-1">
+            <img class="img-1" src="img/about_img_1.jpg"/>
+            <div class="txte-img-1">Texto</div>
+            <div class="txtc-img-1">AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="foot">
+            <nav class="nav nav-pills flex-column flex-sm-row align-items-center justify-content-center" id="navfoot">
+                <a class="flex-sm-fill text-sm-center nav-link" href="index.php">
+                    <img src="img/logob.png" alt="Logo de MHVWare" height="130" width="130">
+                </a>
+                <a class="flex-sm-fill text-sm-center nav-link text-light" href="index.php">Inicio</a>
+                <a class="flex-sm-fill text-sm-center nav-link text-light" href="about.php">Contactanos</a>
+                <a class="flex-sm-fill text-sm-center nav-link text-light" href="contact.php">Sobre nosotros...</a>
+            </nav>
+            <div class="redes">
+                <ul class="nav justify-content-center">
+                    <li class="nav-item">
+                        <a class="nav-link" id="red" href="#"><i class="fa-brands fa-instagram fa-lg"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="red" href="#"><i class="fa-brands fa-facebook fa-lg"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="red" href="#"><i class="fa-brands fa-x-twitter fa-lg"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="red" href="#"><i class="fa-brands fa-tiktok fa-lg"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="red" href="#"><i class="fa-brands fa-youtube fa-lg"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="red" href="#"><i class="fa-brands fa-linkedin-in fa-lg"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="red" href="#"><i class="fa-brands fa-whatsapp fa-lg"></i></a>
+                    </li>
+                </ul>
+            </div>
+            <br>
+            <div class="derechos">
+                <nav class="navbar">
+                    <a class="navbar-brand text-white">Empresa MHVWare | Todos los derechos reservados &copy;</a>
+                    <span class="badge badge-info">
+                        <?php
+                        date_default_timezone_set('America/Mexico_City');
+                        echo "Ultima modificación: " . date("d/m/Y H:i:s.", getlastmod());
+                        ?>
+                    </span>
+                    <p class="navbar-item my-2 my-lg-0 text-white">Al utilizar nuestro sitio indicas que aceptas nuestro
+                        <a class="text-white-50" href="#">aviso de privacidad</a>
+                    </p>
+                </nav>
+            </div>
+            <br>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"
+        integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
