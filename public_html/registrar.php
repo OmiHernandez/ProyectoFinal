4cfe8870da0b7af13e880442988d23a9dfab6d10
JalkemMorales <jalkemmorales@gmail.com>
2023-12-02 00:23:36 -0600

    Registrarse

--- public_html/registrar.php
+++ public_html/registrar.php
@@ -1,9 +1,9 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="css/registrar.css">
 <link rel="stylesheet" href="css/menu.css">
-<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
 <div class="log">
-    <img src="img/logo.png" alt="Logo MHVWare" height="200" width="200">
+    <img src="img/logo.png" alt="Logo Botanical" height="200" width="200">
 </div>
 <?php
 
@@ -32,90 +32,215 @@ if ($_POST["metodo"] == "registrar") {
     $pregunta = $_POST["pregunta"];
     $respuesta = $_POST["respuesta"];
 
-    $sql = 'select * from cuenta where Usuario="'.$usuario.'"';
+    $hash = password_hash($contra, PASSWORD_DEFAULT);
+
+    $sql = 'select * from cuenta where Usuario="' . $usuario . '"';
     $resultado = $conexion->query($sql);
 
     if ($resultado->num_rows) {
-        ?>
+?>
+
         <div class="alert alert-danger" role="alert" id="alerta">
-                    <h4 class="alert-heading">Error de inicio de sesión</h4>
-                    <p>Error. Usuario ya existe.</p>
-                    <hr>
-                    <h6 class="mb-0">Esta siendo redireccionado.</h6>
+            <h4 class="alert-heading">Error.</h4>
+            <p>El usuario ya existe.</p>
+            <hr>
+            <h6 class="mb-0">Esta siendo redireccionado.</h6>
+        </div>
+        <br>
+        <footer>
+            <div class="foot">
+                <nav class="nav nav-pills flex-column flex-sm-row align-items-center justify-content-center" id="navfoot">
+                    <a class="flex-sm-fill text-sm-center nav-link" href="index.php">
+                        <img src="img/logoWF.png" alt="Logo de BotanicalG" height="130" width="130">
+                    </a>
+                    <a class="flex-sm-fill text-sm-center nav-link text-light" href="index.php">Inicio</a>
+                    <a class="flex-sm-fill text-sm-center nav-link text-light" href="about.php">Contactanos</a>
+                    <a class="flex-sm-fill text-sm-center nav-link text-light" href="contact.php">Sobre nosotros...</a>
+                </nav>
+                <div class="redes">
+                    <ul class="nav justify-content-center">
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-instagram fa-lg"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-facebook fa-lg"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-x-twitter fa-lg"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-tiktok fa-lg"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-youtube fa-lg"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-linkedin-in fa-lg"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-whatsapp fa-lg"></i></a>
+                        </li>
+                    </ul>
                 </div>
                 <br>
-                <footer>
-                    <div class="foot">
-                        <nav class="nav nav-pills flex-column flex-sm-row align-items-center justify-content-center" id="navfoot">
-                            <a class="flex-sm-fill text-sm-center nav-link" href="index.php">
-                                <img src="img/logob.png" alt="Logo de MHVWare" height="130" width="130">
-                            </a>
-                            <a class="flex-sm-fill text-sm-center nav-link text-light" href="index.php">Inicio</a>
-                            <a class="flex-sm-fill text-sm-center nav-link text-light" href="about.php">Contactanos</a>
-                            <a class="flex-sm-fill text-sm-center nav-link text-light" href="contact.php">Sobre nosotros...</a>
-                        </nav>
-                        <div class="redes">
-                            <ul class="nav justify-content-center">
-                                <li class="nav-item">
-                                    <a class="nav-link" id="red" href="#"><i class="fa-brands fa-instagram fa-lg"></i></a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" id="red" href="#"><i class="fa-brands fa-facebook fa-lg"></i></a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" id="red" href="#"><i class="fa-brands fa-x-twitter fa-lg"></i></a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" id="red" href="#"><i class="fa-brands fa-tiktok fa-lg"></i></a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" id="red" href="#"><i class="fa-brands fa-youtube fa-lg"></i></a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" id="red" href="#"><i class="fa-brands fa-linkedin-in fa-lg"></i></a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" id="red" href="#"><i class="fa-brands fa-whatsapp fa-lg"></i></a>
-                                </li>
-                            </ul>
-                        </div>
-                        <br>
-                        <div class="derechos">
-                            <nav class="navbar">
-                                <a class="navbar-brand text-white">Empresa MHVWare | Todos los derechos reservados &copy;</a>
-                                <span class="badge badge-info">
-                                    <?php
-                                    date_default_timezone_set('America/Mexico_City');
-                                    echo "Ultima modificación: " . date("d/m/Y H:i:s.", getlastmod());
-                                    ?>
-                                </span>
-                                <p class="navbar-item my-2 my-lg-0 text-white">Al utilizar nuestro sitio indicas que aceptas nuestro <a class="text-white-50" href="#">aviso de privacidad</a></p>
-                            </nav>
-                        </div>
-                        <br>
-                    </div>
-                </footer>
+                <div class="derechos">
+                    <nav class="navbar">
+                        <a class="navbar-brand text-white">Empresa BotanicalG | Todos los derechos reservados &copy;</a>
+                        <span class="badge badge-info">
+                            <?php
+                            date_default_timezone_set('America/Mexico_City');
+                            echo "Ultima modificación: " . date("d/m/Y H:i:s.", getlastmod());
+                            ?>
+                        </span>
+                        <p class="navbar-item my-2 my-lg-0 text-white">Al utilizar nuestro sitio indicas que aceptas nuestro <a class="text-white-50" href="#">aviso de privacidad</a></p>
+                    </nav>
+                </div>
+                <br>
+            </div>
+        </footer>
+    <?php
+        header("refresh:6;url=index.php");
+        exit();
+    }
+
+    $sql = 'select * from cuenta where Correo="' . $correo . '"';
+    $resultado = $conexion->query($sql);
+
+    if ($resultado->num_rows) {
+    ?>
+
+        <div class="alert alert-danger" role="alert" id="alerta">
+            <h4 class="alert-heading">Error.</h4>
+            <p>Este correo ya esta siendo utilizado.</p>
+            <hr>
+            <h6 class="mb-0">Esta siendo redireccionado.</h6>
+        </div>
+        <br>
+        <footer>
+            <div class="foot">
+                <nav class="nav nav-pills flex-column flex-sm-row align-items-center justify-content-center" id="navfoot">
+                    <a class="flex-sm-fill text-sm-center nav-link" href="index.php">
+                        <img src="img/logoWF.png" alt="Logo de BotanicalG" height="130" width="130">
+                    </a>
+                    <a class="flex-sm-fill text-sm-center nav-link text-light" href="index.php">Inicio</a>
+                    <a class="flex-sm-fill text-sm-center nav-link text-light" href="about.php">Contactanos</a>
+                    <a class="flex-sm-fill text-sm-center nav-link text-light" href="contact.php">Sobre nosotros...</a>
+                </nav>
+                <div class="redes">
+                    <ul class="nav justify-content-center">
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-instagram fa-lg"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-facebook fa-lg"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-x-twitter fa-lg"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-tiktok fa-lg"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-youtube fa-lg"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-linkedin-in fa-lg"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-whatsapp fa-lg"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <br>
+                <div class="derechos">
+                    <nav class="navbar">
+                        <a class="navbar-brand text-white">Empresa BotanicalG | Todos los derechos reservados &copy;</a>
+                        <span class="badge badge-info">
+                            <?php
+                            date_default_timezone_set('America/Mexico_City');
+                            echo "Ultima modificación: " . date("d/m/Y H:i:s.", getlastmod());
+                            ?>
+                        </span>
+                        <p class="navbar-item my-2 my-lg-0 text-white">Al utilizar nuestro sitio indicas que aceptas nuestro <a class="text-white-50" href="#">aviso de privacidad</a></p>
+                    </nav>
+                </div>
+                <br>
+            </div>
+        </footer>
         <?php
         header("refresh:6;url=index.php");
         exit();
     }
 
-    $sql = 'select * from cuenta where Correo="'.$correo.'"';
-    $resultado = $conexion->query($sql);
-
-    if ($resultado->num_rows) {
-        
-        header("refresh:6;url=index.php");
-        exit();
-    }
-
     $sql = "INSERT INTO cuenta
-          VALUES(default, '$usuario', '$correo', '$contra', '$pregunta', '$respuesta', '$nombre', default)";
+          VALUES(default, '$usuario', '$correo', '$hash', '$pregunta', '$respuesta', '$nombre', default)";
     $resultado = $conexion->query($sql);
 
     $_SESSION["nombre"] = $usuario;
     $_SESSION["correo"] = $correo;
-    
+
+    ?>
+
+        <div class="alert alert-success" role="alert" id="alerta">
+            <h4 class="alert-heading">Registro exitoso.</h4>
+            <p>Se ha registrado correctamente.</p>
+            <hr>
+            <h6 class="mb-0">Esta siendo redireccionado.</h6>
+        </div>
+        <br>
+        <footer>
+            <div class="foot">
+                <nav class="nav nav-pills flex-column flex-sm-row align-items-center justify-content-center" id="navfoot">
+                    <a class="flex-sm-fill text-sm-center nav-link" href="index.php">
+                        <img src="img/logoWF.png" alt="Logo de BotanicalG" height="130" width="130">
+                    </a>
+                    <a class="flex-sm-fill text-sm-center nav-link text-light" href="index.php">Inicio</a>
+                    <a class="flex-sm-fill text-sm-center nav-link text-light" href="about.php">Contactanos</a>
+                    <a class="flex-sm-fill text-sm-center nav-link text-light" href="contact.php">Sobre nosotros...</a>
+                </nav>
+                <div class="redes">
+                    <ul class="nav justify-content-center">
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-instagram fa-lg"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-facebook fa-lg"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-x-twitter fa-lg"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-tiktok fa-lg"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-youtube fa-lg"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-linkedin-in fa-lg"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="red" href="#"><i class="fa-brands fa-whatsapp fa-lg"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <br>
+                <div class="derechos">
+                    <nav class="navbar">
+                        <a class="navbar-brand text-white">Empresa BotanicalG | Todos los derechos reservados &copy;</a>
+                        <span class="badge badge-info">
+                            <?php
+                            date_default_timezone_set('America/Mexico_City');
+                            echo "Ultima modificación: " . date("d/m/Y H:i:s.", getlastmod());
+                            ?>
+                        </span>
+                        <p class="navbar-item my-2 my-lg-0 text-white">Al utilizar nuestro sitio indicas que aceptas nuestro <a class="text-white-50" href="#">aviso de privacidad</a></p>
+                    </nav>
+                </div>
+                <br>
+            </div>
+        </footer>
+        <?php
+
     header("refresh:6;url=index.php");
     exit();
 } else if ($_POST["metodo"] == "iniciar") {
@@ -130,7 +255,7 @@ if ($_POST["metodo"] == "registrar") {
         $palabra = fscanf($handle, "%s%s%s");
         if ($palabra) {
             if ($palabra[0] == $usuario && $palabra[2] == $contra) {
-    ?>
+        ?>
                 <br>
                 <div class="alert alert-success" role="alert" id="alerta">
                     <h4 class="alert-heading">¡Inicio de sesion exitoso!</h4>
